2b2426863e5f969a6d8dc43bded48fac69fcc996
Morgan Hamet <morganhamet@gmail.com>
2022-07-12 18:23:17 -0700

    Add other query functions

--- src/getWows.js
+++ src/getWows.js
@@ -4,4 +4,30 @@ async function getRandomWow() {
     return data;
 }
 
-export { getRandomWow };
+async function getWowByMovie(movie) {
+    const response = await fetch(`https://owen-wilson-wow-api.herokuapp.com/wows/random?movie=${movie}`, {mode: "cors"});
+    const data = await response.json();
+    return data;
+}
+
+async function getWowByYear(year) {
+    const response = await fetch(`https://owen-wilson-wow-api.herokuapp.com/wows/random?year=${year}`, {mode: "cors"});
+    const data = await response.json();
+    return data;
+}
+
+// Doesn't work??
+async function getWowByCharacter(character) {
+    const response = await fetch(`https://owen-wilson-wow-api.herokuapp.com/wows/random?character=${character}`, {mode: "cors"});
+    const data = await response.json();
+    return data;
+}
+
+// Also doesn't work
+async function getWowByNumber(number) {
+    const response = await fetch(`https://owen-wilson-wow-api.herokuapp.com/wows/random?wow_in_movie=${number}`, {mode: "cors"});
+    const data = await response.json();
+    return data;
+}
+
+export { getRandomWow, getWowByMovie, getWowByYear, getWowByCharacter, getWowByNumber};
--- src/index.js
+++ src/index.js
@@ -1,34 +1,68 @@
 import './style.css';
-import { getRandomWow } from './getWows';
+import { getRandomWow, getWowByMovie, getWowByYear, getWowByCharacter, getWowByNumber } from './getWows';
 
-const randomButton = document.getElementById("random-wow");
+const randomButton = document.getElementById("random-button");
+const searchButton = document.getElementById("search-button");
 const main = document.querySelector("main");
 const container = document.createElement("div");
 
-async function loadRandomWow() {
+async function getWow(wowFunction, parameter) {
     try {
-        const data = await getRandomWow();
-        displayRandomWow(data);
+        const data = await wowFunction(parameter);
+        displayWow(data);
     } catch(error) {
         alert("error");
     }
 }
 
-function displayRandomWow(data) {
+function displayWow(data) {
     container.innerHTML = "";
+
     const movie = document.createElement("h2");
     movie.textContent = `${data[0].movie} (${data[0].year})`;
+
     const director = document.createElement("h3");
     director.textContent = `Directed by ${data[0].director}`;
+
     const character = document.createElement("h3");
     character.textContent = `Playing: ${data[0].character}`;
+
+    const line = document.createElement("p");
+    line.textContent = `"${data[0].full_line}"`;
+
+    const numberOfWows = document.createElement("p");
+    numberOfWows.textContent = `Total wows: ${data[0].total_wows_in_movie}`;
+
     const video = document.createElement("video");
     video.controls = "controls";
     const source = document.createElement("source");
     source.src = data[0].video["1080p"];
     video.appendChild(source);
-    container.append(movie, director, character, video);
+    container.append(movie, director, character, line, numberOfWows, video);
     main.appendChild(container);
 }
 
-randomButton.onclick = loadRandomWow;
+randomButton.onclick = () => getWow(getRandomWow, "");
+
+searchButton.onclick = () => {
+    const categories = document.getElementById("categories");
+    const input = document.getElementById("search");
+    let category = categories.value;
+    let searchValue = input.value;
+
+    switch(category) {
+        case "movie":
+            getWow(getWowByMovie, searchValue);
+            break;
+        case "year":
+            getWow(getWowByYear, searchValue);
+            break;
+        case "character":
+            getWow(getWowByCharacter, searchValue);
+            break;
+        case "number": 
+            getWow(getWowByNumber, searchValue);
+            break;
+    }
+}
+
