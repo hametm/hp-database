e9237bea9204973f08f846f9dc42157e5f24c022
Morgan Hamet <morganhamet@gmail.com>
2022-07-13 01:05:00 -0700

    Improve UI and add mobile-friendly version

--- src/index.js
+++ src/index.js
@@ -1,60 +1,47 @@
 import './style.css';
 import { getRandomWow, getWowByMovie, getWowByYear, getWowByDirector, getWowByOccurrence } from './getWows';
-import Owen from "./images/owen-flipped.png";
 
 const randomButton = document.getElementById("random-button");
 const searchButton = document.getElementById("search-button");
 const wowContainer = document.getElementById("wow-container")
 const container = document.createElement("div");
 container.classList.add("container");
+wowContainer.appendChild(container);
 
 async function getWow(wowFunction, parameter) {
     try {
         const data = await wowFunction(parameter);
         displayWow(data);
     } catch(error) {
-        alert("error");
+        const errorMessage = document.createElement("p");
+        errorMessage.textContent = "Wow! We couldn't find any matches."
+        container.appendChild(errorMessage);
     }
 }
 
 function displayWow(data) {
     container.innerHTML = "";
-    container.classList.add("container");
 
-    const movie = document.createElement("h2");
-    movie.textContent = `${data[0].movie} (${data[0].year})`;
+    const topRow = document.createElement("h2");
+    topRow.classList.add("topRow");
+    const bottomRow = document.createElement("p");
+    bottomRow.classList.add("bottomRow");
 
-    const director = document.createElement("h3");
-    director.textContent = `Directed by ${data[0].director}`;
+    topRow.textContent = `${data[0].movie} (${data[0].year}) |
+            Directed by ${data[0].director} |
+            Role: ${data[0].character}`;
 
-    const character = document.createElement("h3");
-    character.textContent = `Character: ${data[0].character}`;
-
-    const line = document.createElement("p");
-    line.textContent = `Full quote: "${data[0].full_line}"`;
-
-    const occurrence = document.createElement("p");
-    occurrence.textContent = `Wow ${data[0].current_wow_in_movie} of ${data[0].total_wows_in_movie}`;
+    bottomRow.textContent = `Full quote (wow ${data[0].current_wow_in_movie} of ${data[0].total_wows_in_movie}):
+            "${data[0].full_line}"`;
 
     const audio = document.createElement("audio");
     const source = document.createElement("source");
-    // audio.controls = "controls";
+    audio.controls = "controls";
     audio.autoplay = "autoplay";
     audio.src = data[0].audio;
-
-    const owenImage = document.createElement("img");
-    owenImage.src = Owen;
-    owenImage.alt = "";
-
-    // const video = document.createElement("video");
-    // video.controls = "controls";
-    // const source = document.createElement("source");
-    // source.src = data[0].video["1080p"];
-    
-    // video.appendChild(source);
     audio.appendChild(source);
-    container.append(movie, director, character, line, occurrence, audio);
-    wowContainer.append(container, owenImage);
+
+    container.append(topRow, bottomRow, audio);
 }
 
 randomButton.onclick = () => getWow(getRandomWow, "");
